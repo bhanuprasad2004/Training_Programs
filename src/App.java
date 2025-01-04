70767e3dc6274e25589734f0bf1bb7b00a8ecb9e
bhanuprasad2004 <bhanuprasadadunuri@gmail.com>
2025-01-04 10:18:30 +0530

    Files added

---
+++ src/App.java
@@ -0,0 +1,28 @@
+// public class App {
+//     public static void main(String[] args) throws Exception {
+//         System.out.println("Hello, World!");
+//         display();
+//     }
+//     static void display() {
+//         System.out.println("Display");
+        
+//     }
+// }
+
+public class App {
+
+    public static void main(String[] args) {
+        //int a = 10;
+        System.out.println(Byte.MIN_VALUE);
+        System.out.println(Byte.MAX_VALUE);
+        System.out.println(Integer.MIN_VALUE);
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println("MAx value of Interger: "+ (Integer.MAX_VALUE + 1));
+        System.out.println(Character.MIN_VALUE);
+        System.out.println(Character.MAX_VALUE);
+        System.out.println(Float.MIN_VALUE);
+        System.out.println(Float.MAX_VALUE);
+        System.out.println(Double.MIN_VALUE);
+        System.out.println(Double.MAX_VALUE);
+    }
+}
---
+++ src/Day1.java
@@ -0,0 +1,14 @@
+public class Day1 {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 20;
+
+        if (x < y) {
+            System.out.println("X is Greater than Y");
+        } else {
+            System.out.println("X is Less than Y");
+        }
+        
+    }
+}
+
